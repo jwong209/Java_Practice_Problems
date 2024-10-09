4376e58c0e451ff96a27a6b8f6a41f6334b17680
jwong209 <68797448+jwong209@users.noreply.github.com>
2024-10-09 15:51:49 -0700

    Setup String2

--- coding-bat/src/main/java/org/example/Functional1.java
+++ coding-bat/src/main/java/org/example/Functional1.java
@@ -25,7 +25,7 @@ public class Functional1 {
 
     /* >>> addStar <<<
         Given a list of strings, return a list where each string has "*" added at its end.
-        
+
         addStar(["a", "bb", "ccc"]) → ["a*", "bb*", "ccc*"]
         addStar(["hello", "there"]) → ["hello*", "there*"]
         addStar(["*"]) → ["**"]
