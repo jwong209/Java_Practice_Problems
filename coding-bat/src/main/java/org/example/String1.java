ec877aeddf246948713a07404be1ddc8ee06c690
jwong209 <68797448+jwong209@users.noreply.github.com>
2024-10-09 13:55:00 -0700

    Initial setup for problems separated by class

---
+++ coding-bat/src/main/java/org/example/String1.java
@@ -0,0 +1,73 @@
+package org.example;
+
+public class String1 {
+    public static void main(String[] args) {
+        String1 string1 = new String1();
+        System.out.println("helloName: "+ string1.helloName("Bob"));
+        System.out.println("helloName: "+ string1.helloName("Alice"));
+        System.out.println("helloName: "+ string1.helloName("X"));
+    }
+
+    /* >>> helloName <<<
+        Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+        helloName("Bob") → "Hello Bob!"
+        helloName("Alice") → "Hello Alice!"
+        helloName("X") → "Hello X!"
+    */
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+     /* >>> xxx <<<
+
+    */
+
+}
