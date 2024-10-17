4294a9806dafe82b0e5fe5888773215fbacb1daf
jwong209 <68797448+jwong209@users.noreply.github.com>
2024-10-17 10:52:33 -0700

    Added to Array1 solutions up to sum3

--- coding-bat/src/main/java/org/example/Array1.java
+++ coding-bat/src/main/java/org/example/Array1.java
@@ -1,10 +1,41 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Array1 {
     public static void main(String[] args) {
         Array1 array1 = new Array1();
 
-        System.out.println();
+//        System.out.println("firstLast6: " + array1.firstLast6(new int[]{1, 2, 6}));
+//        System.out.println("firstLast6: " + array1.firstLast6(new int[]{6, 1, 2, 3}));
+//        System.out.println("firstLast6: " + array1.firstLast6(new int[]{13, 6, 1, 2, 3}));
+
+//        System.out.println("sameFirstLast: " + array1.sameFirstLast(new int[]{1, 2, 3}));
+//        System.out.println("sameFirstLast: " + array1.sameFirstLast(new int[]{1, 2, 3, 1}));
+//        System.out.println("sameFirstLast: " + array1.sameFirstLast(new int[]{1, 2, 1}));
+
+//        System.out.println("makePi: " + Arrays.toString(array1.makePi()));
+
+//        System.out.println("commonEnd: " + array1.commonEnd(new int[]{1, 2, 3}, new int[]{7, 3}));
+//        System.out.println("commonEnd: " + array1.commonEnd(new int[]{1, 2, 3}, new int[]{7, 3, 2}));
+//        System.out.println("commonEnd: " + array1.commonEnd(new int[]{1, 2, 3}, new int[]{1, 3}));
+
+//        System.out.println("sum3: " + array1.sum3(new int[]{1, 2, 3}));
+//        System.out.println("sum3: " + array1.sum3(new int[]{5, 11, 2}));
+//        System.out.println("sum3: " + array1.sum3(new int[]{7, 0, 0}));
+
+        System.out.println("");
+
+        System.out.println("");
+
+        System.out.println("");
+
+        System.out.println("");
+
+        System.out.println("");
+
+        System.out.println("");
+
     }
 
     /* >>> firstLast6 <<<
@@ -14,6 +45,12 @@ public class Array1 {
         firstLast6([6, 1, 2, 3]) → true
         firstLast6([13, 6, 1, 2, 3]) → false
     */
+    public boolean firstLast6(int[] nums) {
+        if (nums[0] == 6 || nums[nums.length - 1] == 6) {
+            return true;
+        }
+        return false;
+    }
 
      /* >>> sameFirstLast <<<
         Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
@@ -22,12 +59,25 @@ public class Array1 {
         sameFirstLast([1, 2, 3, 1]) → true
         sameFirstLast([1, 2, 1]) → true
     */
+     public boolean sameFirstLast(int[] nums) {
+         if (nums.length < 1) {
+             return false;
+         }
+         if (nums[0] == (nums[nums.length - 1])) {
+             return true;
+         }
+         return false;
+     }
 
      /* >>> makePi <<<
         Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
 
         makePi() → [3, 1, 4]
     */
+     public int[] makePi() {
+         int[] answer = new int[] {3, 1, 4};
+         return answer;
+     }
 
      /* >>> commonEnd <<<
         Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
@@ -36,6 +86,14 @@ public class Array1 {
         commonEnd([1, 2, 3], [7, 3, 2]) → false
         commonEnd([1, 2, 3], [1, 3]) → true
     */
+     public boolean commonEnd(int[] a, int[] b) {
+         if (a[a.length - 1] == b[b.length - 1]) {
+             return true;
+         } else if (a[0] == b[0]) {
+             return true;
+         }
+         return false;
+     }
 
      /* >>> sum3 <<<
         Given an array of ints length 3, return the sum of all the elements.
@@ -44,6 +102,14 @@ public class Array1 {
         sum3([5, 11, 2]) → 18
         sum3([7, 0, 0]) → 7
     */
+     public int sum3(int[] nums) {
+         int sum = 0;
+         for (int i = 0; i < nums.length; i++) {
+             sum += nums[i];
+         }
+         return sum;
+     }
+
 
      /* >>> rotateLeft3 <<<
         Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
