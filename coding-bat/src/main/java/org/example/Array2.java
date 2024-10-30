8f836e98f10de1f9c8dc9261184e1d20e3c20c92
jwong209 <68797448+jwong209@users.noreply.github.com>
2024-10-30 10:50:56 -0700

    Added to Array2 solutions up to isEverywhere

--- coding-bat/src/main/java/org/example/Array2.java
+++ coding-bat/src/main/java/org/example/Array2.java
@@ -46,8 +46,21 @@ public class Array2 {
 //        System.out.println("fizzArray: " + Arrays.toString(fizzArray(1)));
 //        System.out.println("fizzArray: " + Arrays.toString(fizzArray(10)));
 
-        System.out.println("");
+//        System.out.println("only14: " + only14(new int[]{1, 4, 1, 4}));
+//        System.out.println("only14: " + only14(new int[]{1, 4, 2, 4}));
+//        System.out.println("only14: " + only14(new int[]{1, 1}));
 
+//        System.out.println("fizzArray2: " + Arrays.toString(fizzArray2(4)));
+//        System.out.println("fizzArray2: " + Arrays.toString(fizzArray2(10)));
+//        System.out.println("fizzArray2: " + Arrays.toString(fizzArray2(2)));
+
+//        System.out.println("no14: " + no14(new int[]{1, 2, 3}));
+//        System.out.println("no14: " + no14(new int[]{1, 2, 3, 4}));
+//        System.out.println("no14: " + no14(new int[]{2, 3, 4}));
+
+        System.out.println("isEverywhere: " + isEverywhere(new int[]{1, 2, 1, 3}, 1));
+        System.out.println("isEverywhere: " + isEverywhere(new int[]{1, 2, 1, 3}, 2));
+        System.out.println("isEverywhere: " + isEverywhere(new int[]{1, 2, 1, 3, 4}, 1));
     }
 
     /* >>> countEvens <<<
@@ -256,6 +269,14 @@ public class Array2 {
         only14([1, 4, 2, 4]) → false
         only14([1, 1]) → true
      */
+    public static boolean only14(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 1 && nums[i] != 4) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     /* >>> fizzArray2 <<<
         Given a number n, create and return a new string array of length n, containing the strings "0", "1" "2" .. through n-1. N may be 0, in which case just return a length 0 array. Note: String.valueOf(xxx) will make the String form of most types. The syntax to make a new string array is: new String[desired_length]  (See also: FizzBuzz Code)
@@ -264,6 +285,15 @@ public class Array2 {
         fizzArray2(10) → ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
         fizzArray2(2) → ["0", "1"]
      */
+    public static String[] fizzArray2(int n) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = Integer.toString(i);
+        }
+
+        return answer;
+    }
 
     /* >>> no14 <<<
         Given an array of ints, return true if it contains no 1's or it contains no 4's.
@@ -272,6 +302,22 @@ public class Array2 {
         no14([1, 2, 3, 4]) → false
         no14([2, 3, 4]) → true
      */
+    public static boolean no14(int[] nums) {
+        int count1 = 0;
+        int count4 = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count1++;
+            } else if (nums[i] == 4) {
+                count4++;
+            }
+        }
+        if (count1 > 0 && count4 > 0) {
+            return false;
+        }
+        return true;
+    }
 
     /* >>> isEverywhere <<<
         We'll say that a value is "everywhere" in an array if for every pair of adjacent elements in the array, at least one of the pair is that value. Return true if the given value is everywhere in the array.
@@ -280,6 +326,14 @@ public class Array2 {
         isEverywhere([1, 2, 1, 3], 2) → false
         isEverywhere([1, 2, 1, 3, 4], 1) → false
      */
+    public static boolean isEverywhere(int[] nums, int val) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != val && nums[i + 1] != val) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     /* >>> either24 <<<
         Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.
