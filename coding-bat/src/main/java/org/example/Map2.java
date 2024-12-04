edad4e14cca0205580631b4a2823c69bc46186a1
jwong209 <68797448+jwong209@users.noreply.github.com>
2024-12-04 13:10:28 -0800

    Added allSwap to Map2

--- coding-bat/src/main/java/org/example/Map2.java
+++ coding-bat/src/main/java/org/example/Map2.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -31,11 +32,13 @@ public class Map2 {
 //        System.out.println("wordAppend: " + wordAppend(new String[]{"a", "b", "a", "c", "a", "d", "a"}));
 //        System.out.println("wordAppend: " + wordAppend(new String[]{"a", "", "a"}));
 
-        System.out.println("wordMultiple: " + wordMultiple(new String[]{"a", "b", "a", "c", "b"}));
-        System.out.println("wordMultiple: " + wordMultiple(new String[]{"c", "b", "a"}));
-        System.out.println("wordMultiple: " + wordMultiple(new String[]{"c", "c", "c", "c"}));
+//        System.out.println("wordMultiple: " + wordMultiple(new String[]{"a", "b", "a", "c", "b"}));
+//        System.out.println("wordMultiple: " + wordMultiple(new String[]{"c", "b", "a"}));
+//        System.out.println("wordMultiple: " + wordMultiple(new String[]{"c", "c", "c", "c"}));
 
-        System.out.println("allSwap: ");
+        System.out.println("allSwap: " + Arrays.toString(allSwap(new String[]{"ab", "ac"})));
+        System.out.println("allSwap: " + Arrays.toString(allSwap(new String[]{"ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"})));
+        System.out.println("allSwap: " + Arrays.toString(allSwap(new String[]{"ax", "bx", "ay", "by", "ai", "aj", "bx", "by"})));
 
         System.out.println("firstSwap: ");
     }
@@ -188,6 +191,23 @@ public class Map2 {
         allSwap(["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"]) → ["ay", "by", "cy", "cx", "bx", "ax", "azz", "aaa"]
         allSwap(["ax", "bx", "ay", "by", "ai", "aj", "bx", "by"]) → ["ay", "by", "ax", "bx", "aj", "ai", "by", "bx"]
      */
+    public static String[] allSwap(String[] strings) {
+        String[] result = new String[strings.length];
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < strings.length; i++) {
+            char c = strings[i].charAt(0);
+            if (map.containsKey(c)) {
+                int p = map.get(c);
+                map.remove(c);
+                result[i] = result[p];
+                result[p] = strings[i];
+            } else {
+                result[i] = strings[i];
+                map.put(c, i);
+            }
+        }
+        return result;
+    }
 
     /* >>> firstSwap <<<
         We'll say that 2 strings "match" if they are non-empty and their first chars are the same. Loop over and then return the given array of non-empty strings as follows: if a string matches an earlier string in the array, swap the 2 strings in the array. A particular first char can only cause 1 swap, so once a char has caused a swap, its later swaps are disabled. Using a map, this can be solved making just one pass over the array. More difficult than it looks.
